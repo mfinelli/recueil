e596a77d5c629cabddd2397902e87fc37ea34bf3
Mario Finelli <mario@finel.li>
2026-07-10 17:23:46 -0400

    Add initial eslint config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["terraform/index.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.serviceworker },
+    },
+  },
+  {
+    files: ["terraform/tests/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.serviceworker, ...globals.vitest },
+    },
+  },
+];
